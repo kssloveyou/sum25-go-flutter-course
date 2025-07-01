664ba23875d5d043363938f431e8a14c6906ca40
Ksenia <ksenia@MacBook-Air-von-Ksenia.local>
2025-07-01 18:03:47 +0300

    lab01:all tasks complete

--- labs/lab01/backend/calculator/calculator.go
+++ labs/lab01/backend/calculator/calculator.go
@@ -31,9 +31,8 @@ func Divide(a, b float64) (float64, error) {
 	// TODO: Implement this function
 	if b == 0 {
 		return 0, ErrDivisionByZero
-	} else {
-		return a / b, nil
 	}
+	return a / b, nil
 }
 
 // StringToFloat converts a string to float64
